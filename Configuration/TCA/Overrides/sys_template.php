ceef26ec4171e2acea3c2d4079d2a88a89ce28fd
Maik Kempe <mkempe@bitaculous.com>
2021-09-21 21:18:12 +0200

    [➠] Cloned from T3v Blueprint. [❶] First commit.

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The `sys_template` TCA override.
+ *
+ * @noinspection PhpFullyQualifiedNameUsageInspection
+ */
+
+defined('TYPO3_MODE') or die();
+
+// === Variables ===
+
+$extensionKey = 't3v_backend';
+$extensionTitle = 'T3v Backend';
+
+// === TypoScript ===
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extensionKey, 'Configuration/TypoScript', $extensionTitle);
+
+// === T3v Generator ===
